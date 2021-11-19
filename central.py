5b9522bcfc97d868b3483c54b4716d035f566d89
Noah <noahagdam@gmail.com>
2021-11-19 09:59:53 +0100

    oi

--- central.py
+++ central.py
@@ -1,2 +1,16 @@
 from socket import socket, gethostbyname, AF_INET, SOCK_DGRAM
 from threading import Thread as T
+
+s = socket(AF_INET, SOCK_DGRAM)
+s.connect(('8.8.8.8', 1))
+local_ip_address = s.getsockname()[0]
+print("Running Central Communication Server")
+print("Your ip   :", local_ip_address)
+s.close()
+
+PORT_NUMBER = 5000
+SIZE = 1024
+hostName = gethostbyname('0.0.0.0')
+mySocket = socket( AF_INET, SOCK_DGRAM )
+mySocket.bind( (hostName, PORT_NUMBER) )
+
